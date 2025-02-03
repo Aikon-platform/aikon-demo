acf96e3da1ead0001e11c5117e531538c5c59aae
Segolene-Albouy <segolene.albouy@gmail.com>
2025-02-03 22:55:49 +0100

    [SETUP] improve scripts

--- scripts/venv.sh
+++ scripts/venv.sh
@@ -2,9 +2,21 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source "$SCRIPT_DIR/utils.sh"
 
-color_echo  "SET UP VIRTUAL ENVIRONMENTS"
-python3 -m venv "$API_DIR"/venv
-python3 -m venv "$FRONT_DIR"/venv
+for ver in 3.{12..10}; do
+    if command -v "python$ver" &> /dev/null; then
+        python="python$ver"
+        break
+    fi
+done
+
+if [ -z "$python" ]; then
+    color_echo red "Python 3.10 or higher is required"
+    exit 1
+fi
+
+color_title "SET UP VIRTUAL ENVIRONMENTS"
+$python -m venv "$API_DIR"/venv
+$python -m venv "$FRONT_DIR"/venv
 
 for venv in "$API_VENV" "$FRONT_VENV"; do
     "$venv"/pip install --upgrade pip
