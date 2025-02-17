dfc84d42f0536a2fc008cdaac5020e383ef940f5
Segolene-Albouy <segolene.albouy@gmail.com>
2025-02-17 15:17:20 +0100

    [FIX] remove merge relicate + similarity results processing

--- front/similarity/models.py
+++ front/similarity/models.py
@@ -8,9 +8,14 @@ from regions.models import AbstractAPITaskOnCrops
 
 
 class Similarity(AbstractAPITaskOnCrops("similarity")):
-    def save_similarity(self, similarity: dict):
+    def save_similarity(self, output: dict):
         if not self.dataset:
             return
+
+        # TODO save annotations using results_url instead
+        # output.get("results_url", [])
+        similarity = output.get("similarity", {})
+
         with open(self.task_full_path / f"{self.dataset.id}.json", "wb") as f:
             f.write(orjson.dumps(similarity))
         self._similarity = similarity
@@ -36,9 +41,7 @@ class Similarity(AbstractAPITaskOnCrops("similarity")):
             if not output:
                 self.on_task_error({"error": "No output data"})
                 return
-
-            # TODO download annotations file
-            # self.save_similarity(output.get("annotations", {}))
+            self.save_similarity(output)
 
             dataset_url = output.get("dataset_url")
             if dataset_url:
