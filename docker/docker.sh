d36edd4ab5edccb5b9b4da9194120f8b13e8d3b4
Segolene-Albouy <segolene.albouy@gmail.com>
2025-03-20 12:12:05 +0100

    [EOLE] add network config

--- docker/docker.sh
+++ docker/docker.sh
@@ -13,7 +13,9 @@ dc="docker-compose -p aikondemo"
 build_containers() {
     # initialize the .env and config files as well as data folder permissions on first initialization
     bash "$DOCKER_DIR"/init.sh
+    docker network disconnect -f aikondemo_demo_network aikonapi
     $dc build
+    docker network connect aikondemo_demo_network aikonapi
     # TODO collectstatic
 }
 
