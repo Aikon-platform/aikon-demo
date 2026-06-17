da9eea42d29060405fd535b26a79ac424bdeeb16
paulhectork <paul.kervegan@hotmail.fr>
2026-06-17 15:25:42 +0200

    [FORMS] pass Watermark Index name to Svelte

--- front/interface/src/WatermarksApp/components/IndexSelect.svelte
+++ front/interface/src/WatermarksApp/components/IndexSelect.svelte
@@ -1,18 +1,23 @@
 <script lang="ts">
     import { ToggleGroup } from "bits-ui";
     import { onMount } from "svelte";
+    import { enforceValue, updateUrlSearchParams } from "../../shared/utils";
 
     interface Props {
         options: HTMLLabelElement[];
         value: string;
     }
 
+    const urlParamName = "index";
+
     let { options, value = $bindable() }:Props = $props();
 
+    // TODO bind to this parameter.
     const options_detailed = $derived(options.map(option => ({
         label: Array.from(option.childNodes).filter(node => (node as any).tagName !== "INPUT").map(node => (node as any).outerHTML || node.textContent).join(""),
         input: option.querySelector("input") as HTMLInputElement,
     })));
+    console.log("input_ids", options_detailed.map(o => o.input.id));
 
     function onValueChange(newValue: string) {
         options_detailed.forEach(({ input }) => {
@@ -22,6 +27,7 @@
                 input.checked = false;
             }
         });
+        console.log("onValueChange", newValue);
     }
 
     onMount(() => {
